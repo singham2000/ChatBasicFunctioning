1de3c678affab39b63b2e2ed83baae9f0a542b4e
Surya <thakursuryapratap9@gmail.com>
2022-07-26 03:47:44 +0530

    Final working

---
+++ script.js
@@ -0,0 +1,33 @@
+const socket = io('http://localhost:3000');
+
+const messageForm = document.getElementById('send-container');
+const messageContainer = document.getElementById('message-container');
+const messageInput = document.getElementById('message-input');
+const name = prompt('Your Name');
+appendMessage('You Joined');
+socket.emit('new-user', name);
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name} : ${data.message}`);
+})
+socket.on('user-connected', name => {
+    appendMessage(`${name} Connected`);
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} Disconnected`);
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message);
+    messageInput.value = ' ';
+});
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const io = require('socket.io')(3000);
+const users = {};
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete [socket.id];
+    })
+    socket.emit('chat-message', 'welcome');
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+})
