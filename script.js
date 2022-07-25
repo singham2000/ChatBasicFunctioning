49f31b5062a692ad9c1dd69d7cbe38f58c69681e
Surya <thakursuryapratap9@gmail.com>
2022-07-26 03:57:07 +0530

    Work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://demo-chatty-socketio.herokuapp.com/');
 
 const messageForm = document.getElementById('send-container');
 const messageContainer = document.getElementById('message-container');
